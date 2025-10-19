f62f1d5ac907c5606b215f1aedf7e6600644391d
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2025-10-19 18:33:22 +0200

    ci: run tuist generate post clone

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,4 +8,7 @@
 curl https://mise.run | MISE_VERSION=$(MISE_VERSION) sh
 echo "eval \"\$(/Users/local/.local/bin/mise activate zsh)\"" >> "/Users/local/.zshrc"
 source /Users/local/.zshrc
+mise settings experimental=true
 mise install
+
+tuist generate
--- ci_scripts/ci_pre_xcodebuild.sh
+++
@@ -1,8 +0,0 @@
-#
-//  ci_pre_xcodebuild.sh
-//  Cartesia
-//
-//  Created by Antonio on 19/10/25.
-//
-
-tuist generate

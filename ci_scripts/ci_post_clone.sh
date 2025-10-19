aa81f72c6511f9179ec5e2d21a1ba7c4ae2499f8
Antonio Pantaleo <antonio_pantaleo@icloud.com>
2025-10-19 19:01:31 +0200

    ci: improve post clone script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,14 +1,15 @@
-#
-//  ci_post_clone.sh
-//  Cartesia
-//
-//  Created by Antonio on 19/10/25.
-//
+#!/bin/bash
+
+set -euo pipefail
+set -x
+
+export TUIST_STATS_OPT_OUT=1
+export TUIST_CONFIG_CLOUD_ANALYTICS_OPT_OUT=1
 
 curl https://mise.run | MISE_VERSION=$(MISE_VERSION) sh
-echo "eval \"\$(/Users/local/.local/bin/mise activate zsh)\"" >> "/Users/local/.zshrc"
+echo "eval \"\$(/Users/local/.local/bin/mise activate zsh)\"" >>"/Users/local/.zshrc"
 source /Users/local/.zshrc
 mise settings experimental=true
 mise install
 
-mise exec -- tuist generate -p ..
+mise exec -- tuist generate --no-open -p ..
